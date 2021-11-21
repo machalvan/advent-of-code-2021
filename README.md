5547f5a9060e4ba41dac486faa541b95a1e8698d
Marcus Hansson <machalvan@hotmail.com>
2021-11-21 22:19:03 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2021
+
+https://adventofcode.com/2021
+
+Solutions written in Python 3
---
+++ day-template/main.py
@@ -0,0 +1,17 @@
+def part1(input):
+
+    return
+
+
+def part2(input):
+
+    return
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        file_str = f.read()
+        file = file_str.strip().split('\n')
+
+    print(part1(list(file)))
+    print(part2(list(file)))
