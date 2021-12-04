709e7bf58b770b4624280fd6f71c54afd4134494
Marcus Hansson <machalvan@hotmail.com>
2021-12-04 09:06:52 +0100

    Add transpose to utils

--- utils.py
+++ utils.py
@@ -1,2 +1,11 @@
 def int_list(str_list):
     return list(map(int, str_list))
+
+
+def transpose(matrix):
+    """Transpose a matrix
+    Examples:
+        >>> transpose([[1, 2, 3], [4, 5, 6]])
+        [[1, 4], [2, 5], [3, 6]]
+    """
+    return list(map(list, zip(*matrix)))
