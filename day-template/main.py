8f532e382a787647a253f24b887829f12862db4c
Marcus Hansson <machalvan@hotmail.com>
2021-12-17 16:53:48 +0100

    Update template

--- day-template/main.py
+++ day-template/main.py
@@ -1,16 +1,20 @@
-def part1(lines):
+def part1(input):
+    lines = input.split('\n')
+    blocks = [block.split('\n') for block in input.split('\n\n')]
+
+    print('input:', input)
+    print('lines:', lines)
+    print('blocks:', blocks)
 
     return
 
 
-def part2(lines):
+def part2(input):
 
     return
 
 
 if __name__ == '__main__':
-    with open('input.txt') as f:
-        lines = [line.strip() for line in f]
-
-    print(part1(lines))
-    print(part2(lines))
+    input = open('input.txt').read().strip()
+    print(part1(input))
+    print(part2(input))
